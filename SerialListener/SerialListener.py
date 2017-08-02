896083458fdfb5cefd03c48ad9b59eb0c8352819
Jacob Taylor Cassady <jtcass01@louisville.edu>
2017-08-01 22:40:01 -0400

    Baseline serial communication

--- SerialListener/SerialListener.py
+++ SerialListener/SerialListener.py
@@ -1,1 +1,9 @@
+import serial, time
 
+serial_port = serial.Serial('COM6', 9600, timeout=0)
+
+while True:
+    try:
+        print(serial_port.readline())
+    except serial_port.SerialTimeoutException:
+        print('Data could not be read.')
