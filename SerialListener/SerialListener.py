a13ddc5613a7025e75a9fa6eb5ba9953f446d77c
Jacob Taylor Cassady <jtcass01@louisville.edu>
2017-08-03 09:13:57 -0400

    Tested and ready to go : )

--- SerialListener/SerialListener.py
+++ SerialListener/SerialListener.py
@@ -1,33 +1,37 @@
 import serial, time, logging, datetime
 
-#serial_port = serial.Serial('COM6', 9600, timeout=0)
+#Don't forget to update the serial port location!!
+serial_port = serial.Serial('COM6', 9600, timeout=0)
 
 header = ["time", "response"]
 start = time.time()
-data_file_string = 'RCRSerialListener{}.dat'.format(datetime.datetime.now().strftime("%y_%m_%d_%H_%M"))
-data_file = open(data_file_string, "w")
-print("Data file initialized.")
 
-def store(info):
+def store(file, info):
+    file.write(info + "\n")
+
+def format_and_store(file, data):
+    store(file, ",".join(data))
+
+
+def main(serial_port):
+    data_file_string = 'RCRSerialListener{}.dat'.format(datetime.datetime.now().strftime("%y_%m_%d_%H_%M"))
     data_file = open(data_file_string, "w")
-    data_file.write(info + "\n")
-    data_file.close()
 
-def format_and_store(data):
-    print(data)
-    store(",".join(data))
+    format_and_store(data_file, header)
+    print("Data file initialized.")
 
-format_and_store(header)
+    try:
+        while True:
+            try:
+                response = serial_port.readline()
+                run_time = str(time.time() - start)
+                format_and_store(data_file, [run_time, response])
+                print("Stored:",run_time,response)
+            except serial_port.SerialTimeoutException:
+                print('Data could not be read.')
+                format_and_store(data_file, [run_time, "No data found."])
+    except KeyboardInterrupt:
+        data_file.close()
+        print("File closed.")
 
-try:
-#       response = serial_port.readline()
-    response = "test"
-    run_time = time.time() - start
-    format_and_store([run_time, response])
-    print("Stored:",run_time,response)
-except KeyboardInterrupt:
-    print("File closed.")
-#    except serial_port.SerialTimeoutException:
-#        print('Data could not be read.')
-
-data_file.close()
+main(serial_port)
